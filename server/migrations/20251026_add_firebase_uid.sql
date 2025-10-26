baf551a26c16ce20f866d050fdb239833d12c051
Idan Shani <IdanS@proceed.co.il>
2025-10-26 12:29:12 +0200

    Add firebase_uid column to student_profiles

---
+++ server/migrations/20251026_add_firebase_uid.sql
@@ -0,0 +1,9 @@
+﻿-- Add firebase_uid to student_profiles
+ALTER TABLE student_profiles 
+ADD COLUMN IF NOT EXISTS firebase_uid VARCHAR(255);
+
+ALTER TABLE student_profiles
+ADD COLUMN IF NOT EXISTS grade VARCHAR(20);
+
+CREATE INDEX IF NOT EXISTS idx_student_profiles_firebase_uid 
+ON student_profiles(firebase_uid);
